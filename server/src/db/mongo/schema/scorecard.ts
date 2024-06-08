5aeb37bab7a146f4efb79bb2815c1597b50f958f
Fahad Khan <kfahad5607@gmail.com>
2024-06-08 21:28:44 +0530

    Updated scorecard schema

--- server/src/db/mongo/schema/scorecard.ts
+++ server/src/db/mongo/schema/scorecard.ts
@@ -2,65 +2,89 @@ import { Schema, model } from "mongoose";
 import { DBIdType, DBIdUniqueType, PositiveNumberType } from "../schemaTypes";
 import { DISMISSAL_TYPES_VALUES } from "../../../helpers/constants";
 
-const scorecardBatterSchema = new Schema({
-  batterId: DBIdType,
-  batRuns: PositiveNumberType,
-  ballsPlayed: PositiveNumberType,
-  dotBalls: PositiveNumberType,
-  batFours: PositiveNumberType,
-  batSixes: PositiveNumberType,
-  fallOfWicket: new Schema({
-    dismissalType: {
-      type: String,
-      enum: DISMISSAL_TYPES_VALUES,
-    },
+const scorecardBatterSchema = new Schema(
+  {
+    id: DBIdType,
+    batRuns: PositiveNumberType,
+    ballsPlayed: PositiveNumberType,
+    dotBalls: PositiveNumberType,
+    batFours: PositiveNumberType,
+    batSixes: PositiveNumberType,
+    fallOfWicket: new Schema({
+      dismissalType: {
+        type: String,
+        enum: DISMISSAL_TYPES_VALUES,
+      },
+      overs: PositiveNumberType,
+      teamScore: PositiveNumberType,
+      teamWickets: PositiveNumberType,
+      bowlerId: DBIdType,
+      helpers: [DBIdType],
+    }),
+  },
+  { _id: false }
+);
+
+const scorecardBowlerSchema = new Schema(
+  {
+    id: DBIdType,
+    bowlOvers: PositiveNumberType,
+    bowlMaidens: PositiveNumberType,
+    bowlRuns: PositiveNumberType,
+    bowlWickets: PositiveNumberType,
+    bowlWides: PositiveNumberType,
+    bowlNoBalls: PositiveNumberType,
+  },
+  { _id: false }
+);
+
+const extraBallSchema = new Schema(
+  {
+    nos: PositiveNumberType,
+    wides: PositiveNumberType,
+    legByes: PositiveNumberType,
+    byes: PositiveNumberType,
+    penalties: PositiveNumberType,
+  },
+  { _id: false }
+);
+
+const scorecardInningsSchema = new Schema(
+  {
+    teamId: DBIdType,
+    inningsId: DBIdType,
     overs: PositiveNumberType,
-    teamScore: PositiveNumberType,
-    teamWickets: PositiveNumberType,
-    bowlerId: DBIdType,
-    helpers: [DBIdType],
-  }),
-});
+    oversBowled: PositiveNumberType,
+    score: PositiveNumberType,
+    wickets: PositiveNumberType,
+    isDeclared: Boolean,
+    isFollowOn: Boolean,
+    batters: [scorecardBatterSchema],
+    bowlers: [scorecardBowlerSchema],
+    extras: extraBallSchema,
+    // add target
+  },
+  { _id: false }
+);
 
-const scorecardBowlerSchema = new Schema({
-  bowlerId: DBIdType,
-  bowlOvers: PositiveNumberType,
-  bowlMaidens: PositiveNumberType,
-  bowlRuns: PositiveNumberType,
-  bowlWickets: PositiveNumberType,
-  bowlWides: PositiveNumberType,
-  bowlNoBalls: PositiveNumberType,
-});
-
-const extraBallSchema = new Schema({
-  nos: PositiveNumberType,
-  wides: PositiveNumberType,
-  legByes: PositiveNumberType,
-  byes: PositiveNumberType,
-  penalties: PositiveNumberType,
-});
+const scorecardInningsObjSchema = new Schema(
+  {
+    first: scorecardInningsSchema,
+    second: scorecardInningsSchema,
+    third: scorecardInningsSchema,
+    fourth: scorecardInningsSchema,
+  },
+  { _id: false }
+);
 
 const scorecardSchema = new Schema({
   matchId: DBIdUniqueType,
-  innings: [
-    new Schema(
-      {
-        teamId: DBIdType,
-        inningsId: DBIdType,
-        overs: PositiveNumberType,
-        oversBowled: PositiveNumberType,
-        score: PositiveNumberType,
-        wickets: PositiveNumberType,
-        isDeclared: Boolean,
-        isFollowOn: Boolean,
-        batters: [scorecardBatterSchema],
-        bowlers: [scorecardBowlerSchema],
-        extras: extraBallSchema,
-        // add target
-      },
-      { _id: false }
-    ),
-  ],
+  // innings: [scorecardInningsSchema],
+  innings: {
+    type: scorecardInningsObjSchema,
+    default: {},
+    required: true,
+  },
 });
 
 const Scorecard = model("Scorecard", scorecardSchema);
