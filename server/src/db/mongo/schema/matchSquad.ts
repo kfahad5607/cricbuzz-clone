d270948c66b07a3e27ce92b364cc9e0400ab175e
Fahad Khan <kfahad5607@gmail.com>
2024-06-09 22:29:13 +0530

    Added typescript to matchsquad schema

--- server/src/db/mongo/schema/matchSquad.ts
+++ server/src/db/mongo/schema/matchSquad.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { DBIdType, DBIdUniqueType } from "../schemaTypes";
+import { MatchSquad, MatchSquadPlayer } from "../../../types";
 
 const squadPlayerSchema = new Schema(
   {
@@ -17,7 +18,7 @@ const squadPlayerSchema = new Schema(
   { _id: false }
 );
 
-const matchSquadsSchema = new Schema({
+const matchSquadsSchema = new Schema<MatchSquad<MatchSquadPlayer>>({
   matchId: DBIdUniqueType,
   teams: [
     new Schema(
@@ -30,6 +31,9 @@ const matchSquadsSchema = new Schema({
   ],
 });
 
-const MatchSquads = model("MatchSquad", matchSquadsSchema);
+const MatchSquads = model<MatchSquad<MatchSquadPlayer>>(
+  "MatchSquad",
+  matchSquadsSchema
+);
 
 export default MatchSquads;
