7253ba36cf58562407790364065debf011ef5eef
Fahad Khan <kfahad5607@gmail.com>
2024-06-20 23:24:37 +0530

    Added basic seeding for teams

--- server/src/db/postgres/seeds/index.ts
+++ server/src/db/postgres/seeds/index.ts
@@ -1,9 +1,11 @@
 import seedVenues from "./venues";
 import seedSeries from "./series";
+import seedTeams from "./teams";
 
 const main = async () => {
   // await seedVenues();
-  await seedSeries();
+  // await seedSeries();
+  await seedTeams();
 };
 
 main();
---
+++ server/src/db/postgres/seeds/teams.ts
@@ -0,0 +1,37 @@
+import * as fs from "node:fs/promises";
+import { db, client } from "../../postgres";
+import * as tables from "../../postgres/schema";
+import slugify from "slugify";
+
+const seedTeams = async () => {
+  try {
+    console.log("Seeding teams started...");
+    const contents = await fs.readFile(
+      "src/db/postgres/seeds/data/teams/index.json",
+      { encoding: "utf8" }
+    );
+
+    const data = JSON.parse(contents);
+
+    const teams: any[] = [];
+    for (const key in data) {
+      const item = data[key];
+      delete item.id;
+
+      item.slug = slugify(item.name, {
+        lower: true,
+      });
+      teams.push(item);
+    }
+
+    await db.insert(tables.teams).values(teams);
+
+    client.end();
+
+    console.log("Seeding teams finished...");
+  } catch (err) {
+    console.error("ERROR in seeding teams ==> ", err);
+  }
+};
+
+export default seedTeams;
